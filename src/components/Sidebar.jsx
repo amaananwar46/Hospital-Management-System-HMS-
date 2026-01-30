4a18fbcb1711b667499f1eca4075841ed603b4e2
amaananwar46 <amaananwar46@gmail.com>
2026-01-30 16:19:13 +0530

    Made appointment form mobile-first responsive UI

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,60 @@
+import { useState } from "react";
 import "../stylesheets/Sidebar.css";
-export default function Sidebar({setActiveModule}) {
+
+export default function Sidebar({ setActiveModule }) {
+
+  const [activeBtn, setActiveBtn] = useState("patients");
+
+  const handleClick = (module) => {
+    setActiveBtn(module);
+    setActiveModule(module);
+  };
+
   return (
-    <div className='sidebar'>   
-      <button onClick={() => setActiveModule("patients")}>Patients</button>
-      <button onClick={() => setActiveModule("appointments")}>Appointments</button>
-      <button onClick={() => setActiveModule("doctors")}>Doctors</button>
-      <button onClick={() => setActiveModule("inventory")}>Inventory</button>
-      <button onClick={() => setActiveModule("medicine-shop")}>Buy Medicine</button>
-      <button onClick={() => setActiveModule("purchase-history")}>Purchase History</button>
-      </div>
-  )
+    <div className="sidebar">
+
+      <button
+        className={activeBtn === "patients" ? "active" : ""}
+        onClick={() => handleClick("patients")}
+      >
+        Patients
+      </button>
+
+      <button
+        className={activeBtn === "appointments" ? "active" : ""}
+        onClick={() => handleClick("appointments")}
+      >
+        Appointments
+      </button>
+
+      <button
+        className={activeBtn === "doctors" ? "active" : ""}
+        onClick={() => handleClick("doctors")}
+      >
+        Doctors
+      </button>
+
+      <button
+        className={activeBtn === "inventory" ? "active" : ""}
+        onClick={() => handleClick("inventory")}
+      >
+        Inventory
+      </button>
+
+      <button
+        className={activeBtn === "medicine-shop" ? "active" : ""}
+        onClick={() => handleClick("medicine-shop")}
+      >
+        Buy Medicine
+      </button>
+
+      <button
+        className={activeBtn === "purchase-history" ? "active" : ""}
+        onClick={() => handleClick("purchase-history")}
+      >
+        Purchase History
+      </button>
+
+    </div>
+  );
 }
